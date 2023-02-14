abaca18fcab25aeaea09fee79229532184912493
Tyler Stuckey <tylerwstuckey@gmail.com>
2023-02-14 12:17:32 -0600

    implemented reverseCharacters

--- StringLoops.java
+++ StringLoops.java
@@ -1,10 +1,23 @@
-public class Loops {
+public class StringLoops {
 
   // static methods go here
 
-  public static void main(String[] args) {
 
-    // testing of static methods goes here
+  public static String reverseCharacters(String str) {
+    String word = "";
+    for (int pos = str.length(); pos > 0; pos--) {
+      word = word + str.substring(pos-1, pos);
+    }
+    return word;
+    
+    
+    
+    
+  }
+
+  public static void main(String[] args) {
+  
+  System.out.println(reverseCharacters("chicken"));
 
   }
 }
